7f266694d28279e855ada796248f528b7ccf5dfc
JunHyeok Seo <sjhgd107@naver.com>
2023-07-24 17:08:58 +0900

    chap07_Class

---
+++ src/chap_02/_Quiz_02.java
@@ -0,0 +1,9 @@
+package chap_02;
+
+public class _Quiz_02 {
+	public static void main(String[] args) {
+		int height = 115;
+		String s = height >= 120 ? "가능" : "불가능";
+		System.out.printf("키가 %dcm 이므로 탑승이 %s합니다", height, s);
+ 	}
+}
